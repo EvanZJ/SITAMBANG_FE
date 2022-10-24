c351c4371747379fd71efff5687e414a2be81c8d
unknown <januar.evan2012@gmail.com>
2022-10-24 22:47:20 +0700

    feat: new StockSeller view

---
+++ src/components/NavBarPenjual.vue
@@ -0,0 +1,91 @@
+<template>
+    <nav>
+        <div v-if="menu">
+            <svg @click="toggleMenu" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100" height="100" fill="#028AFF"/>
+            <path d="M45.8333 25L45.8333 75L54.1667 75L54.1667 25L45.8333 25ZM25 25L25 75L33.3333 75L33.3333 25L25 25ZM75 25L66.6667 25L66.6667 75L75 75L75 25Z" fill="white"/>
+            </svg>
+        </div>
+        <div v-else>
+            <svg @click="toggleMenu" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100" height="100" fill="#028AFF"/>
+            <path d="M25 54.1667H75V45.8333H25V54.1667ZM25 75H75V66.6667H25V75ZM25 25V33.3333H75V25H25Z" fill="white"/>
+            </svg>
+        </div>
+        <h2>
+            SITAMBANG
+        </h2>
+    </nav>
+    <div class="menu-list" v-if="menu">
+        <div class="row gx-5">
+            <div class="col">
+                <router-link to="">Riwayat Transaksi</router-link>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col">
+                <router-link to="">Kondisi Peralatan Tambak</router-link>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col">
+                <router-link to="">Informasi Barang</router-link>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col">
+                <router-link to="">Verifikasi Pemesanan</router-link>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col">
+                <router-link to="">Data Karyawan</router-link>
+            </div>
+        </div>
+        <div class="row gx-5">
+            <div class="col">
+                <router-link to="">Data Pembeli</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            menu: false,
+        }
+    },
+    methods:{
+        toggleMenu(){
+            this.menu = !this.menu
+        }
+    }
+}
+</script>
+
+<style scoped>
+    nav{
+        display: flex;
+        width: 100%;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        background-color: #028aff;
+        color: white;
+    }
+    .menu-list{
+        display: flex;
+        background-color: #028aff;
+        color: white;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: flex-start;
+        width: fit-content;
+        padding: 10px;
+    }
+    router-link{
+        color: white;
+    }
+</style>
