aeb47950388d473ad4c0d7107d6776a55fe116ae
Haniif Ahmad Jauhari <100585249+HaniifAJ@users.noreply.github.com>
2022-10-24 20:31:18 +0700

    Login, Register, and User Home

---
+++ src/views/LoginView.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav>
+        <h2>SITAMBANG</h2>
+        <div>
+            <router-link to='/login'>Login</router-link>
+            <router-link to='/register'>Register</router-link>
+        </div>
+    </nav>
+    <!-- Idk why this not work -->
+    <!-- <PreNavBar /> -->
+    <div>
+        <h2>Login</h2>
+        <form @submit.prevent="$this.emit()">
+            <div>
+                <p>Email</p>
+                <input type="text" v-model="email">
+            </div>
+            <div>
+                <p>Password</p>
+                <input type="password" v-model="password">
+            </div>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</template>
+
+<script>
+import PreNavBar from '../components/PreNavBar.vue'
+
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,57 @@
+<template>
+    <nav>
+        <h2>SITAMBANG</h2>
+        <div>
+            <router-link to='/login'>Login</router-link>
+            <router-link to='/register'>Register</router-link>
+        </div>
+    </nav>
+    <!-- Idk why this not work -->
+    <!-- <PreNavBar /> -->
+    <div>
+        <h2>Form Pendaftaran Pembeli</h2>
+        <form @submit.prevent="$this.emit()">
+            <div>
+                <p>Nama</p>
+                <input type="text" v-model="nama">
+            </div>
+            <div>
+                <p>Nomor Telepon</p>
+                <input type="text" v-model="notelp">
+            </div>
+            <div>
+                <p>Email</p>
+                <input type="text" v-model="email">
+            </div>
+            <div>
+                <p>Password</p>
+                <input type="password" v-model="password">
+            </div>
+            <div>
+                <p>Konfirmasi Password</p>
+                <input type="text" v-model="confirmpw">
+            </div>
+            <input type="submit" value="Daftar">
+        </form>
+    </div>
+</template>
+
+<script>
+import PreNavBar from '../components/PreNavBar.vue'
+
+export default {
+    data(){
+        return{
+            nama: '',
+            notelp: '',
+            email: '',
+            password: '',
+            confirmpw: '',
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
